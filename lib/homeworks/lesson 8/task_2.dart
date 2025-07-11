fcaa1b2c1e46ec662fbb560696dfd2328c67c022
boryskurit <boryskuritsyn@gmail.com>
2025-07-12 00:51:35 +0300

    master corrections made

--- lib/homeworks/lesson 8/task_2.dart
+++ lib/homeworks/lesson 8/task_2.dart
@@ -1,8 +1,8 @@
-final Set<String> iremember = {"int", "string", "double", "bool", "list", "map", "set", "object"};
+final Set<String> iremember = {'int', 'string', 'double', 'bool', 'list', 'map', 'set', 'object'};
 
-final Set<String> allthetypes = {"int", "string", "double", "bool", "list", "map", "set", "object", "dynamic", "void", "null", "enum", };
+final Set<String> allthetypes = {'int', 'string', 'double', 'bool', 'list', 'map', 'set', 'object', 'dynamic', 'void', 'null', 'enum', };
 
 void main() {
-  print("Types in both sets: ${allthetypes.intersection(iremember)}");
-  print("Types I do not remember: ${allthetypes.difference(iremember)}");
+  print('Types in both sets: ${allthetypes.intersection(iremember)}');
+  print('Types I do not remember: ${allthetypes.difference(iremember)}');
 }
