ad4859f4cb18b7809bbdc56086de6c388676444c
boryskurit <boryskuritsyn@gmail.com>
2025-07-12 00:26:22 +0300

    Task 8:

--- lib/homeworks/lesson_11/hw_streams.dart
+++ lib/homeworks/lesson_11/hw_streams.dart
@@ -1,10 +1,9 @@
-// Task 6: Стрім з чисел (fromIterable)
-
 import 'dart:async';
 
 void main() async {
 
-  // Task 6: Стрім з чисел (fromIterable)
+  // Task 6: Стрім з чисел (fromIterable).
+
   final Stream<int> numbers = Stream<int>.fromIterable([1, 15, 153, 90, 180, 6, 13, 43, 7, 8]);
 
   print('Await for output:');
@@ -16,4 +15,31 @@ void main() async {
   numbers.listen((n) {
     print(n);
   });
+
+  //Task 7: Зворотний відлік зі стріму (periodic)
+
+  print('\nPeriodic output:');
+  final Stream<int> str = Stream<int>.periodic(Duration(seconds: 1), (x) => x + 1).take(10);
+  str.listen((n) {
+    print('$n...');
+  });
+
+  //Task 8: Робота з StreamController
+
+  final StreamController<String> sc = StreamController<String>();
+  print('\nStreamcontroller output:');
+  sc
+    ..add('Dark side of the Moon')
+    ..add('The Wall')
+    ..add('Wish You Were Here')
+    ..add('Animals')
+    ..add('Meddle');
+
+  sc.stream.listen((s) {
+    print(s);
+  }, onDone: () {
+    print('Стрім завершено');
+  });
+
+  await sc.close();
 }
